9a32b18f2e2c707b74e24a96c2c4cfddd8eca2ac
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-10-11 18:02:31 +0800

    image + edit delete function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
@@ -7,17 +5,40 @@ import BookList from './components/BookList';
 function App() {
   const [books, setBooks] = useState([]);
 
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { ...book, title: newTitle };
+      }
+      return book;
+    });
+
+    setBooks(updatedBooks);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [
       ...books,
-      { id: Math.round(Math.random * 9999), title },
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
     ];
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <h1>Reading List</h1>
+      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
