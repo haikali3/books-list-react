c25ca08e2c992c4c49992b403e4d06c27847a6ea
haikali3 <105930061+haikali3@users.noreply.github.com>
2023-10-13 17:56:14 +0800

    use effect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import BookCreate from './components/BookCreate';
 import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books');
+
+    setBooks(response.data);
+  };
+
+  useEffect(() => {
+    //render once
+    fetchBooks();
+  }, []);
+
+  //render every time
+  // useEffect(() => {
+  //   fetchBooks();
+  // });
+
+  //render when books change
+  // useEffect(() => {
+  //   fetchBooks();
+  // }, [setBooks]]);
 
   const editBookById = (id, newTitle) => {
     const updatedBooks = books.map((book) => {
@@ -24,14 +46,12 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+  const createBook = async (title) => {
+    const response = await axios.post('http://localhost:3001/books', {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
